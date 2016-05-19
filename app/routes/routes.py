21f77a59b5c8466fd692ea682515a84b99f77ba9
bogdana_kuzmenko <bohdana.kuzmenko@introlab-systems.com>
2016-05-19 18:22:01 +0300

    refactored

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -84,3 +84,5 @@ def get_no_extractions_file():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
--- app/services/check_bios/main.py
+++ app/services/check_bios/main.py
@@ -29,46 +29,55 @@ class Extractor(object):
              'specialties', 'score']]
 
     def filter_with_regex(self, bio_df):
-        splitted_bios = concat([Series(row['profileUrl'], sentences_splitter(row['attorneyBio']))
-                                for _, row in bio_df.iterrows()]).reset_index()
-        splitted_bios.columns = ["attorneyBio", "profileUrl"]
-        splitted_bios[['sent_num', 'sentence']] = splitted_bios['attorneyBio'].apply(Series)
+        splitted_bios = self.split_bio_df(bio_df)
 
         unique_content_regexes_keys = set(self.joined_regexes.content_regex.values)
-        content_regexes_dict = DataHandler.df_to_dict(self.content_regexes, "Content REG ID", "KeyWord")
+        all_content_regexes_dict = DataHandler.df_to_dict(self.content_regexes, "Content REG ID", "KeyWord")
 
         result = []
-
-        print("Data filtering started")
         for content_regex_key in unique_content_regexes_keys:
-            content_regex = content_regexes_dict.get(content_regex_key)
-            df_for_content_filtering = splitted_bios.copy()
-            cn_filtered_bios = df_for_content_filtering[
-                df_for_content_filtering['sentence'].str.contains(content_regex)]
+            content_regex_value = all_content_regexes_dict.get(content_regex_key)
+            content_filtered_bios = self.filter_bios(splitted_bios, content_regex_value)
 
             joined_regexes = self.joined_regexes[self.joined_regexes["content_regex"] == content_regex_key][[
                 "regex", "regex_id"]].values.tolist()
             for regex in joined_regexes:
-                regex_value, regex_index = regex
-                df_for_foined_filetering = cn_filtered_bios.copy()
-                regex_df = df_for_foined_filetering[df_for_foined_filetering['sentence'].str.contains(regex_value)]
-                if not regex_df.empty:
-                    current_regex_info_df = self.joined_regexes[self.joined_regexes["regex"] == regex_value]
-                    pr_areas = list(set(current_regex_info_df["pract_areas"].values.tolist()))
-                    score = list(set(current_regex_info_df["score"].values.tolist()))
-                    specialties = list(set(current_regex_info_df["specialties"].values.tolist()))
-                    context_regex = list(set(current_regex_info_df["context_regex"].values.tolist()))
+                joined_regex_value, joined_regex_index = regex
+                bio_df = self.filter_bios(content_filtered_bios, joined_regex_value)
 
-                    regex_df['content'] = DataFrame(
-                        [content_regex_key] * len(regex_df.attorneyBio.values)).values
-                    regex_df['context'] = DataFrame([context_regex] * len(regex_df.attorneyBio.values)).values
-                    regex_df['joined'] = DataFrame([regex_index] * len(regex_df.attorneyBio.values)).values
-                    regex_df['practice_areas'] = DataFrame(pr_areas * len(regex_df.attorneyBio.values)).values
-                    regex_df['specialties'] = DataFrame(specialties * len(regex_df.attorneyBio.values)).values
-                    regex_df['score'] = DataFrame(score * len(regex_df.attorneyBio.values)).values
-                    result.append(regex_df)
+                if not bio_df.empty:
+                    pr_areas, specialties, score, context_regex = self.get_regex_info(joined_regex_value)
 
+                    bio_df['content'] = DataFrame(
+                        [content_regex_key] * len(bio_df.attorneyBio.values)).values
+                    bio_df['context'] = DataFrame([context_regex] * len(bio_df.attorneyBio.values)).values
+                    bio_df['joined'] = DataFrame([joined_regex_index] * len(bio_df.attorneyBio.values)).values
+                    bio_df['practice_areas'] = DataFrame(pr_areas * len(bio_df.attorneyBio.values)).values
+                    bio_df['specialties'] = DataFrame(specialties * len(bio_df.attorneyBio.values)).values
+                    bio_df['score'] = DataFrame(score * len(bio_df.attorneyBio.values)).values
+                    result.append(bio_df)
         return concat(result)
+
+    def get_regex_info(self, regex_value):
+        current_regex_info_df = self.joined_regexes[self.joined_regexes["regex"] == regex_value]
+        pr_areas = list(set(current_regex_info_df["pract_areas"].values.tolist()))
+        specialties = list(set(current_regex_info_df["specialties"].values.tolist()))
+        score = list(set(current_regex_info_df["score"].values.tolist()))
+        context_regex = list(set(current_regex_info_df["context_regex"].values.tolist()))
+        regex_info = (pr_areas, specialties, score, context_regex)
+        return regex_info
+
+    def split_bio_df(self, df):
+        splitted_bios = concat([Series(row['profileUrl'], sentences_splitter(row['attorneyBio']))
+                                for _, row in df.iterrows()]).reset_index()
+        splitted_bios.columns = ["attorneyBio", "profileUrl"]
+        splitted_bios[['sent_num', 'sentence']] = splitted_bios['attorneyBio'].apply(Series)
+        return splitted_bios
+
+    def filter_bios(self, df_to_filter, regex_for_filtering):
+        df_to_filter_copy = df_to_filter.copy()
+        filtered_bios = df_to_filter_copy[df_to_filter_copy['sentence'].str.contains(regex_for_filtering)]
+        return filtered_bios
 
 
 if __name__ == "__main__":
